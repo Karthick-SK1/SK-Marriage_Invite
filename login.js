5d7177551fbd9d00a4b03d6dfb8d59be72904f7d
BotGit00 <karthickshan766@gmail.com>
2023-05-14 11:37:49 +0530

    completed

---
+++ login.js
@@ -0,0 +1,21 @@
+function login() {
+  // Get the values of the username and password input fields.
+  var username = document.querySelector("input[type=text]").value;
+  var password = document.querySelector("input[type=password]").value;
+
+  // Set the expected username and password values.
+  var expectedUsername = "Shahi";
+  var expectedPassword = "papa";
+
+  if (username === expectedUsername && password === expectedPassword) {
+    // The user logged in successfully.
+    alert("Hello Azhagi !!!!");
+    window.location.href = "main.html"; // redirect to blog page
+  } else {
+    // The user did not log in successfully.
+    alert("Nee Yaaru di Mairu...Veliya Podi !!!!");
+  }
+}
+
+// Add an event listener to the Login button to call the login() function when it's clicked.
+document.querySelector("button").addEventListener("click", login);
