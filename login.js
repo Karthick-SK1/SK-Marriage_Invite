5526a3a38706ab49ee175a60f779819a9f0267f0
BotGit00 <karthickshan766@gmail.com>
2023-05-21 18:20:28 +0530

    added blur loading screen

--- login.js
+++ login.js
@@ -7,13 +7,23 @@ function login(event) {
   var expectedPassword = "papa";
 
   var loginMessage = document.querySelector("#login-message"); // Get the login message element
+  var blurredBackground = document.createElement('div');
+  blurredBackground.classList.add('blurred-background');
+  var loadingGif = document.createElement('img');
+  loadingGif.src = 'iphone-loading.gif'; // Replace 'iphone-loading.gif' with the actual path to your GIF file
+  loadingGif.classList.add('loading-gif');
+  blurredBackground.appendChild(loadingGif);
 
   if (username === expectedUsername && password === expectedPassword) {
     loginMessage.textContent = "!!!! Karthick & Shahina Welcomes You !!!!";
     loginMessage.style.color = "green";
-    window.location.href = "indexx.html";
+    setTimeout(function () {
+      window.location.href = "indexx.html"; // Delayed page redirection
+    }, 1000); // 2000ms delay (2 seconds)
+    document.body.appendChild(blurredBackground); // Add the blurred background with loading GIF
   } else {
     loginMessage.textContent = "Sorry !!! You are not allowed in our privacy";
+    loginMessage.style.fontSize = "0.7rem";
     loginMessage.style.color = "red";
   }
 }
